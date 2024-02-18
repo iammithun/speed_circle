a2789ac437bd6436923e505de1eb6b4858037f4e
iammithun <157653522+iammithun@users.noreply.github.com>
2024-02-18 16:51:00 -0500

    Add files via upload

---
+++ speed-circle.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 18 16:44:53 2024
+
+@author: iamrs
+"""
+
+import turtle
+
+def draw_circle(speed):
+
+    t = turtle.Turtle()
+    
+   
+    t.speed(speed)
+
+
+    t.circle(100000000000000000000000000)
+
+    turtle.done()
+
+
+draw_circle(speed=1000000000000)
